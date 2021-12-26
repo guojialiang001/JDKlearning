412011655a5bbe678ffea3d9b47dd22ae2bd56a5
guojialiang <447326870@qq.com>
2021-12-26 17:36:18 +0800

    333

---
+++ src/com/tolearning/Main.java
@@ -0,0 +1,26 @@
+package com.tolearning;
+
+
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        //Object  a =  new Object();
+
+       // System.out.println(a);
+
+        Number n = 0;
+      Class<? extends Number> c = n.getClass();
+
+
+        System.out.println(c);
+
+
+    }
+}
